4916846660bf4b4abad4161b3027d3dcec4bd43e
nguyentp24 <nguyentpse181840@fpt.edu.vn>
2024-10-03 11:48:30 +0700

    lan dau dua code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css'
+import Navigation from './Components/NavigationTemplate/Navigation'
+import { ThemeContext } from "./ThemeContext";
+
+function App() {
+  const [DarkTheme, setDarkTheme] = useState(true)
+  return (
+    <ThemeContext.Provider value={{ DarkTheme, setDarkTheme }}>
+      <div className="App">
+        <Navigation />
+
+        {/* <Main/> */}
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
